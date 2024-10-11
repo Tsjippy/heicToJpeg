e8a06635c72eee13fe1104d79ebca56baa8d6182
Tsjippy <ewaldha@hotmail.com>
2024-10-11 07:21:30 +0100

    added: MODULE_PATH constant

--- php/__module_menu.php
+++ php/__module_menu.php
@@ -3,6 +3,9 @@ namespace SIM\HEICTOJPEG;
 use SIM;
 
 const MODULE_VERSION		= '8.0.0';
+
+DEFINE(__NAMESPACE__.'\MODULE_PATH', plugin_dir_path(__DIR__));
+
 DEFINE(__NAMESPACE__.'\MODULE_SLUG', strtolower(basename(dirname(__DIR__))));
 
 DEFINE(__NAMESPACE__.'\MODULE_PATH', plugin_dir_path(__DIR__));
